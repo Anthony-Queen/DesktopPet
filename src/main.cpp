9b9b6dc5f24d165459867be7364c630107153873
Anthony <189166184+Anthony-Queen@users.noreply.github.com>
2026-07-29 12:17:22 +0200

    Make blank 400x400 window appear

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,31 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        std::cout << "Inititialization Failed: " << SDL_GetError();
+        exit(1);
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Pet", 400, 400, SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_NOT_FOCUSABLE);
+    if(!window) {
+        std::cout << "Encountered error creating window: " << SDL_GetError();
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    while(running) {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            if(event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
