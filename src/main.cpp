325a50c3661f32d6edfd67111624b94b3cc98cb7
Anthony <189166184+Anthony-Queen@users.noreply.github.com>
2026-07-29 14:34:54 +0200

    Got window to work in Hyprland

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,26 @@ int main() {
         exit(1);
     }
 
-    SDL_Window* window = SDL_CreateWindow("Pet", 400, 400, SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_NOT_FOCUSABLE);
+    SDL_Window* window = SDL_CreateWindow("Pet", 150, 200, SDL_WINDOW_ALWAYS_ON_TOP);
     if(!window) {
         std::cout << "Encountered error creating window: " << SDL_GetError();
         SDL_Quit();
         return 1;
     }
 
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    if(!renderer){
+         std::cout << "Renderer creation failed: " << SDL_GetError();
+         SDL_DestroyWindow(window);
+         SDL_Quit();
+         return 1;
+    }
+    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+
+    std::cout << "Window Created!\n";
     bool running = true;
     while(running) {
         SDL_Event event;
@@ -25,6 +38,7 @@ int main() {
         }
     }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
