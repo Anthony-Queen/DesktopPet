9b68a2dfa487a4eb4167f3309c30fc73c96ed762
Anthony <189166184+Anthony-Queen@users.noreply.github.com>
2026-08-06 14:55:52 +0200

    Final-Ish commit?
    So I'm on Wayland, and Wayland really hates to give programs Global coordinates/move windows, so unfortunately this project won't be getting realized anytime soon

--- src/GetCursor/getCursor.cpp
+++ src/GetCursor/getCursor.cpp
@@ -1,14 +1,74 @@
-#pragma once
 #include "getCursor.h"
 #include <SDL3/SDL_mouse.h>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <string>
+
+
+//Hyprland Stuff Hyprrrrr Hyprrrrr
+void getCursorHyprland(float& x, float& y)
+{
+    const char* runtime = std::getenv("XDG_RUNTIME_DIR");
+    const char* signature = std::getenv("HYPRLAND_INSTANCE_SIGNATURE");
+
+    if (!runtime || !signature)
+        return;
+
+    std::string path =
+        std::string(runtime) +
+        "/hypr/" +
+        signature +
+        "/.socket.sock";
+
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    if(sock == -1)
+        return;
+
+    sockaddr_un address{};
+    address.sun_family = AF_UNIX;
+
+    std::strncpy(
+        address.sun_path,
+        path.c_str(),
+        sizeof(address.sun_path) - 1
+    );
+
+    if(connect(sock, (sockaddr*)&address, sizeof(address)) == -1)
+    {
+        close(sock);
+        return;
+    }
+
+    std::string command = "/cursorpos";
+
+    ssize_t sent = send(sock, command.c_str(), command.size(), 0);
+    if(sent == -1){
+        close(sock);
+        return;
+    }
+
+    char buffer[128]{};
+
+    ssize_t size = recv(sock, buffer, sizeof(buffer)-1, 0);
+
+    close(sock);
+
+    if(size <= 0)
+        return;
+
+    std::sscanf(buffer, "%f , %f", &x, &y);
+}
+
 
 void getCursorX11(float &x, float &y){
     SDL_GetGlobalMouseState(&x, &y);
 }
 
-void getCursorHyprland(float &x, float &y){
-    //Use Hyprland mouse socket
-}
 
 void getCursorWindows(float &x, float &y){
     //Code for Window
@@ -23,7 +83,7 @@ void setBackend(std::string Backend, void(*&getCursorPos)(float&, float&)){
         getCursorPos = getCursorX11;
     }
     else if(Backend=="wayland"){
-        //Use Hyprland socket
+        getCursorPos = getCursorHyprland;
     }
     else if(Backend=="cocoa"){
         //MacOS code
